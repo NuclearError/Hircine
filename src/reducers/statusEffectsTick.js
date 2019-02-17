c29beec974330de7ac221344056618ac1bb9c06f
Angela Cullen <esmearmitage@gmail.com>
2019-02-17 12:56:56 +0000

    case where no status effect in place, now handled without error

--- src/reducers/statusEffectsTick.js
+++ src/reducers/statusEffectsTick.js
@@ -2,7 +2,7 @@ import { TICK } from '../actions/tick';
 import { BLEED_OUT } from '../actions/bleeding';
 
 // This came from the status effects file. TODO: abstract this somewhere else
-const isBleeding = effects => effects.find(effect => effect.type === BLEED_OUT);
+const isBleeding = effects => effects.find(effect => effect.type === BLEED_OUT) || { amount: 0 };
 
 const statusEffectsTick = (state, action) => {
   if (action.type !== TICK) {
@@ -12,7 +12,7 @@ const statusEffectsTick = (state, action) => {
     ...state,
     stats: {
       ...state.stats,
-      health: state.stats.health - isBleeding(state.statusEffects).amount, // handle case where there are no status effects!!!
+      health: state.stats.health - isBleeding(state.statusEffects).amount,
     },
   };
 };
