c4e0f63b0985f510526e5e2ab7215058815fefb6
Angela Cullen <esmearmitage@gmail.com>
2019-02-20 23:45:29 +0000

    stubbing out game time based on tick

--- src/components/GameTime/GameTime.js
+++ src/components/GameTime/GameTime.js
@@ -1,10 +1,25 @@
 // var secondLoop = setInterval(incrementOneSecond, 500);
-import React from 'react';
+// import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { tick } from '../../actions/tick';
 
-const GameTime = () => { setInterval(DoATick, 1000) };
+function doATick() {
+  console.log('doATick called');
+  this.props.tick();
+}
 
-GameTime.propTypes = {
+const GameTime = () => {
+  console.log("Hello from GameTime");
+  setInterval(doATick, 1000);
 };
 
-export default GameTime;
+GameTime.propTypes = {
+  tick: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+  tick: () => dispatch(tick()),
+});
+
+export default connect(mapDispatchToProps)(GameTime);
