444f7a57f05ae540617a6bff5f34310a36c5b195
Angela Cullen <angela@hubblehq.com>
2019-03-08 13:45:26 +0000

    adding basic colour scheme variables

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,4 +1,10 @@
+// TODO: use methods similar to scss 'mix' using the primary colour palette instead of flat white/black to provide shading
+
 export default {
-  grey500: '#7B7B7B',
-  grey10: '#fcfcfc',
+  brown: '#5c402e',
+  orange: '#e79f4b',
+  yellow: '#f7e198',
+  grey: '#a3bc9c',
+  teal: '#298e88',
+  navy: '#00495c',
 };
