fe00cfd990a8c4b53f4eb6924594e07be6ae65fb
Angela Cullen <angela@hubblehq.com>
2019-03-08 14:17:35 +0000

    Added sass-like mix function for colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,4 +1,4 @@
-// TODO: use methods similar to scss 'mix' using the primary colour palette instead of flat white/black to provide shading
+import mix from './mix';
 
 export default {
   brown: '#5c402e',
@@ -7,4 +7,12 @@ export default {
   grey: '#a3bc9c',
   teal: '#298e88',
   navy: '#00495c',
+  black: '#171f2a',
+
+  baseBlack: this.black,
+  baseWhite: this.yellow,
+
+  paletteTest1: mix(this.baseWhite, this.teal, 60),
+  paletteTest2: mix(this.baseWhite, this.orange, 80),
+  paletteTest3: mix(this.baseBlack, this.orange),
 };
