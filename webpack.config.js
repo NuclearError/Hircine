9d6ae3bbd36dd3c6031db1e307a8ac662bbd4173
Angela Cullen <angela@hubblehq.com>
2019-03-10 16:43:36 +0000

    updated babel, webpack, etc etc

--- webpack.config.js
+++ webpack.config.js
@@ -11,26 +11,34 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015', 'stage-1'],
-        plugins: ['transform-object-rest-spread'],
-      },
-    },
-    {
-      test: /\.(jpg|png|gif|svg|pdf|ico)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[path][name]-[hash:8].[ext]',
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
         },
-      }],
-    }],
+      },
+      {
+        test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name]-[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
   },
   devServer: {
-    historyApiFallback: true,
     contentBase: './',
+    compress: true,
+    port: 8080,
   },
 };
