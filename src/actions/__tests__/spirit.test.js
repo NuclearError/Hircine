05b252870a0c119ddfac212abb592036a8c594b3
Angela Cullen <angela@hubblehq.com>
2019-04-22 22:03:17 +0100

    Fixed typo in spirit tests

--- src/actions/__tests__/spirit.test.js
+++ src/actions/__tests__/spirit.test.js
@@ -1,14 +1,14 @@
-import { GAIN_SPIIRT, gainSpirit, LOSE_SPIIRT, loseSpirit } from '../spirit';
+import { GAIN_SPIRIT, gainSpirit, LOSE_SPIRIT, loseSpirit } from '../spirit';
 
 describe('spirit actions', () => {
   describe('gaining spirit', () => {
     it('outputs an action with the amount of spirit gained', () => {
-      expect(gainSpirit(12)).toEqual({ type: GAIN_SPIIRT, amount: 12 });
+      expect(gainSpirit(12)).toEqual({ type: GAIN_SPIRIT, amount: 12 });
     });
   });
   describe('losing spirit', () => {
     it('outputs an action with the amount of spirit lost', () => {
-      expect(loseSpirit(20)).toEqual({ type: LOSE_SPIIRT, amount: 20 });
+      expect(loseSpirit(20)).toEqual({ type: LOSE_SPIRIT, amount: 20 });
     });
   });
 });
